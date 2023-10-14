9ea1362bfa6ea81adf8733e125098bcb0d07b7f8
yanuartadityan <yanuartadityan@users.noreply.github.com>
2023-10-14 14:33:44 +0000

    ✅ Ready to clone and code.

--- ml_facepoints/cli.py
+++ ml_facepoints/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for ml_facepoints project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m ml_facepoints` and `$ ml_facepoints `.
 
     This is your program's entry point.
 
